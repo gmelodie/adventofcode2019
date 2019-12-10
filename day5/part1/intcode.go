d1831d13ea34ebc20dd7764a2dd91f4259bbee42
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-12-09 22:47:59 -0300

    Bruno wont let me code

--- day5/part1/intcode.go
+++ day5/part1/intcode.go
@@ -8,8 +8,38 @@ import (
 	"strings"
 )
 
-func execStrip(instructions []int) []int {
-	idx := 0
+type instruction struct {
+	opcode  int
+	nparams int
+	name    string
+}
+
+var instructionTypes = map[int]instruction{
+	1:  {1, 3, "sum"},
+	2:  {2, 3, "mult"},
+	3:  {3, 1, "input"},
+	4:  {4, 1, "output"},
+	5:  {5, 2, "jump-if-true"},
+	6:  {6, 2, "jump-if-false"},
+	7:  {7, 3, "less than"},
+	8:  {8, 3, "equals"},
+	99: {99, 0, "halt"},
+}
+
+func exec(instructions []int, IP int) int {
+	params := getParams(instructions, IP)
+	modes := getModes(instructions, IP)
+}
+
+// Run a sequence of instructions
+func Run(instructions []int) []int {
+	newIP := 0
+	IP := 1
+
+	for IP != newIP {
+		IP = newIP
+		newIP := exec(IP, instructions)
+	}
 
 	for opcode := instructions[idx]; opcode != 99; opcode = instructions[idx] {
 		a := instructions[idx+1]
@@ -30,22 +60,27 @@ func execStrip(instructions []int) []int {
 	return instructions
 }
 
-func main() {
-	var instructions []int
+// LoadRaw reads an intcode program from stdin
+// and saves it to strip and the instructions to
+// an array of instrucitons
+func LoadRaw(rawText string) []int {
+	var strip []int
 
-	// Read input
+	line := strings.Split(rawText, ",")
+	for i := 0; i < len(line); i++ {
+		intnum, _ := strconv.Atoi(line[i])
+		strip = append(strip, intnum)
+	}
+
+	return strip
+}
+
+func main() {
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 
-	line := strings.Split(scanner.Text(), ",")
-	for _, num := range line {
-		intnum, _ := strconv.Atoi(num)
-		instructions = append(instructions, intnum)
-	}
-
-	// Calculate stuff
-	instructions[1] = 12
-	instructions[2] = 2
+	strip := LoadRaw(scanner.Text())
 
 	endStrip := execStrip(instructions)
 
