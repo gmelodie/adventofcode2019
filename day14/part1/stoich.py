4479b8a78965095a9c8b16705098a6c433ee8276
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-12-16 15:00:39 -0300

    Add code placeholder

---
+++ day14/part1/stoich.py
@@ -0,0 +1,14 @@
+import sys
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: python3 stoich.py <input_file>")
+        exit(0)
+
+    # read input
+    # calculate best reaction list
+    # print best reaction list
+    # print amount of ore for one fuel
