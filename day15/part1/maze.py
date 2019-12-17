c5be26afdf8368f073cbf13bbbb98d7f0f8415d8
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-12-17 15:32:04 -0300

    Add day 15 part 1 (python)

--- day15/part1/maze.py
+++ day15/part1/maze.py
@@ -1,7 +1,50 @@
 import sys
 import intcode
 
+def update_pos(pos, direction):
+    if direction == 1:
+        return (pos[0], pos[1]+1)
+    elif direction == 2:
+        return (pos[0], pos[1]-1)
+    elif direction == 3:
+        return (pos[0]-1, pos[1])
+    elif direction == 4:
+        return (pos[0]+1, pos[1])
 
+
+def bfs(instructions):
+    visited = {}
+
+    # queue: (instructions, idx, rboffset, pos, pathlen)
+    queue = []
+    visited[(0,0)] = True
+    queue.append((instructions.copy(), 0, 0, (0,0), 0))
+
+    while True:
+        instr, idx, rboffset, pos, pathlen = queue.pop(0)
+        # print(idx, rboffset, pos, pathlen)
+        for i in range(1, 5):
+            new_pos = update_pos(pos, i)
+            if new_pos in visited:
+                continue
+            else:
+                visited[new_pos] = True
+            # print(pathlen)
+
+            new_instr = instr.copy()
+            new_idx, new_rboffset = intcode.execute(new_instr, \
+                                            startidx=idx, \
+                                            rboffset=rboffset,\
+                                            inputdata=[i])
+            output = intcode.RETVAL
+
+            if output == 2:
+                print(output)
+                return pathlen+1
+            elif output == 1:
+                queue.append((new_instr, new_idx, new_rboffset, new_pos, pathlen+1))
+            elif output == 0:
+                continue
 
 
 if __name__ == "__main__":
@@ -10,5 +53,7 @@ if __name__ == "__main__":
         exit(1)
 
     instructions = intcode.load_instructions(sys.argv[1])
+    print(bfs(instructions))
 
-    print(instructions)
+
+
