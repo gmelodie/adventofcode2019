970b5f616ec131c71403455602c4ac9812fca30d
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-12-17 08:52:59 -0300

    Add working day 14 part 1 (python)

--- day14/part1/stoich.py
+++ day14/part1/stoich.py
@@ -1,6 +1,53 @@
 import sys
+import re
 
 
+def read_reactions(filename):
+    reactions = {}
+    with open(sys.argv[1]) as fp:
+        for line in fp:
+            split_line = re.split(', | => | ', line.strip())
+            reactions[split_line[-1]] = {
+                'quantity': int(split_line[-2]),
+                'ingredients': [],
+            }
+            i = 0
+            while i < len(split_line)-2:
+                reactions[split_line[-1]]['ingredients']\
+                .append((int(split_line[i]), split_line[i+1]))
+                i += 2
+
+    return reactions
+
+
+# Calculate the minimum amount of ORE
+# required to produce <quantity> of <material>
+def cost(quantity, material, reactions, leftover):
+    if material == 'ORE': # base case
+        return quantity
+
+    # Get from leftover
+    while quantity > 0 and leftover[material] > 0:
+        quantity -= 1
+        leftover[material] -= 1
+
+    # Careful: 10000 is just a big number
+    # (might not be big enough)
+    for i in range(10000):
+        if reactions[material]['quantity'] * i >= quantity:
+            break
+
+    leftover[material] += reactions[material]['quantity']*i - quantity
+
+    total_ore = 0
+    for amount, ingredient in reactions[material]['ingredients']:
+        total_ore += cost(i*amount, ingredient, reactions, leftover)
+
+
+    # TODO: calculate leftover
+    print("Will need", total_ore, "to produce", quantity, \
+          "of", material)
+    return total_ore
 
 
 if __name__ == "__main__":
@@ -9,6 +56,12 @@ if __name__ == "__main__":
         exit(0)
 
     # read input
+    reactions = read_reactions(sys.argv[1])
+    print(reactions)
+
+    leftover = {}
+    for material in reactions.keys():
+        leftover[material] = 0
+
     # calculate best reaction list
-    # print best reaction list
-    # print amount of ore for one fuel
+    print(cost(1, 'FUEL', reactions, leftover))
