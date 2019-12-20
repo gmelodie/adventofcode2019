79d0d989c0559dbda512bd22bdee33d6e6201f39
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-12-20 17:28:06 -0300

    Add day 17 part 2 (python)

--- day17/part2/vacuum.py
+++ day17/part2/vacuum.py
@@ -1,6 +1,22 @@
 import sys
 import numpy as np
+import time
 import intcode
+
+
+turn_right = {
+    (0, 1): (1, 0),
+    (1, 0): (0, -1),
+    (0, -1): (-1, 0),
+    (-1, 0): (0, 1),
+}
+
+turn_left = {
+    (0, 1): (-1, 0),
+    (-1, 0): (0, -1),
+    (0, -1): (1, 0),
+    (1, 0): (0, 1),
+}
 
 
 def is_intersection(maze, pos):
@@ -23,45 +39,53 @@ def find_intersections(maze):
     return intersections
 
 
-def turn(maze, pos, direction, visited):
-    nextpos = tuple(map(sum, zip(pos, directions[d])))
-    if maze[nextpos[0]][nextpos[1]] == '#' and \
-            nextpos not in visited:
-        return nextpos
+def is_valid(maze, pos):
+    if pos[0] < 0 or pos[0] >= maze.shape[0] or \
+            pos[1] < 0 or pos[1] >= maze.shape[1]:
+        return False
 
-    return (-1, -1)
+    if maze[pos[0]][pos[1]] != '#':
+        return False
 
+    return True
 
 
 def find_path(maze, startpos):
-    path = []
-    visited = {}
 
-
-    print(startpos)
-
+    path = ['R', 0]
     pos = startpos
-    direction = 'UP'
-    nsteps = 0
+    direction = (0, 1) # initial direction set to RIGHT
+    steps = 0
 
     while True:
-        if pos == (-1, -1):
-            break
-        if nsteps == 1:
-            path.append(direction)
+        nxtpos = tuple(map(sum, zip(pos, direction)))
+        if is_valid(maze, nxtpos):
+            pos = nxtpos
+            path[-1] += 1
 
-        nextpos = tuple(map(sum, zip(pos, directions[direction])))
-        if maze[nextpos[0]][nextpos[1]] == '#' and \
-                nextpos not in visited:
-            pos = nextpos
         else:
-            pos = turn(maze, pos, direction, visited)
+            try_right = turn_right[direction]
+            nxtpos = tuple(map(sum, zip(pos, try_right)))
+            if is_valid(maze, nxtpos):
+                direction = try_right
+                pos = nxtpos
+                path.append('R')
+                path.append(1)
+            else:
+                try_left = turn_left[direction]
+                nxtpos = tuple(map(sum, zip(pos, try_left)))
+                if is_valid(maze, nxtpos):
+                    direction = try_left
+                    pos = nxtpos
+                    path.append('L')
+                    path.append(1)
+                else:
+                    break
+
+    return path
 
 
-    return []
-
-
-def generate_input(maze):
+def get_startpos(maze):
     startpos = (-1, -1)
     for i in range(maze.shape[0]):
         if startpos != (-1, -1):
@@ -70,7 +94,7 @@ def generate_input(maze):
             if maze[i][j] == '^':
                 startpos = (i, j)
                 break
-    path = find_path(maze, startpos)
+    return startpos
 
 
 if __name__ == "__main__":
@@ -101,9 +125,32 @@ if __name__ == "__main__":
     # print(sum(inter[0]*inter[1] for inter in intersections))
 
     # Part2
-    inp = generate_input(maze)
+    startpos = get_startpos(maze)
+    print(find_path(maze, startpos))
+
+    seq = "A,B,A,C,A,B,C,B,C,B"
+    A = "R,8,L,10,L,12,R,4"
+    B = "R,8,L,12,R,4,R,4"
+    C = "R,8,L,10,R,8"
+    ans = [ord(a) for a in '\n'.join([seq, A, B, C])]
+    ans.append(ord('\n'))
+    ans.append(ord('n'))
+    ans.append(ord('\n'))
+
     intcode.OUTPUT = []
     instructions[0] = 2 # wake up robot
-    idx, rboffset = intcode.execute(instructions, \
-                                    inputdata=inp)
 
+    idx = rboffset = 0
+    idx, rboffset = intcode.execute(instructions, \
+                                    startidx=idx, \
+                                    rboffset=rboffset, \
+                                    inputdata=ans)
+
+    for c in intcode.OUTPUT:
+        if c == 10:
+            print()
+        else:
+            print(chr(c), end='')
+
+    print()
+    print(intcode.RETVAL)
