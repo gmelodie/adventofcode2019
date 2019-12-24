29e1c3f9b09f956d5617996772d884c97c337d04
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-12-24 19:05:44 -0300

    Add shell code to create day

---
+++ new_day.sh
@@ -0,0 +1,15 @@
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 <day> <name>"
+    exit 1
+fi
+
+mkdir day$1
+
+mkdir day$1/part1
+touch day$1/part1/$2.py
+touch day$1/part1/$2_input.txt
+
+mkdir day$1/part2
+touch day$1/part2/$2.py
+touch day$1/part2/$2_input.txt
